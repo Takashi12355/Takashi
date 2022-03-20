7d695fdc953a74cdbb73dfd8ee7e809d2a7357f3
Takashi12355 <61079056+Takashi12355@users.noreply.github.com>
2022-03-20 10:47:42 +0300

    Delegate

---
+++ Events/Events/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = new KeyBoard();
+            key.Run();
+        }
+        public class KeyBoard
+        {
+            public event EventHandler<char> OnKeyPressed;
+            public void Run()
+            {
+                char input;
+                do
+                {
+                    input = Console.ReadKey().KeyChar;
+                    Console.WriteLine();
+                    OnKeyPressed?.Invoke(this, input);
+                }
+                while (input != 'c');
+            }
+        }
+    }
+}
